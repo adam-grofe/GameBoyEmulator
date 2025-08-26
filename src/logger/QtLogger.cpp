b4e6bb0d400d01fd216df6b85e032f1df085e35a
Adam Grofe <agrofe1417@gmail.com>
2025-08-26 06:11:09 -0700

    Feature/logger interface (#10)
    * Add logger interface
    
    * Update gitignore to include .vscode directory.
    
    * Update cmakelists to make it more modular
    
    * Add changes suggested by copilot

---
+++ src/logger/QtLogger.cpp
@@ -0,0 +1,21 @@
+
+#include <QtLogging>
+#include <QDebug>
+#include "logger/QtLogger.hpp"
+
+
+void Logger::QtLogger::Debug(std::string_view s){
+    qDebug() << QString::fromUtf8(s.data(), s.size());
+}
+
+void Logger::QtLogger::Info(std::string_view s) {
+    qInfo() << QString::fromUtf8(s.data(), s.size());
+}
+
+void Logger::QtLogger::Warn(std::string_view s) {
+    qWarning() << QString::fromUtf8(s.data(), s.size());
+}
+
+void Logger::QtLogger::Error(std::string_view s) {
+    qCritical() << QString::fromUtf8(s.data(), s.size());
+}
